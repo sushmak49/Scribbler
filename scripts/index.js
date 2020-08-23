164b678041c5eefe66cdb52c2f8b8fd71171cd6d
sushmak49 <sushmak49@gmail.com>
2020-08-23 23:58:00 +0530

    Code to re-direct to postslist html file

--- scripts/index.js
+++ scripts/index.js
@@ -5,3 +5,7 @@ function showCreatePostModal() {
 function hideCreatePostModal(){
     document.getElementById('post-modal').style.display="none";
 }
+
+function showAllPosts(){
+    window.location.href = "./html/postslist.html";
+}
